06216b9fd35fd0b9756746a780f67f898460dfcc
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-01-27 15:26:28 +0530

    Refactor Heatmap component to improve readability and performance

--- client/src/components/dashboard/Heatmap.tsx
+++ client/src/components/dashboard/Heatmap.tsx
@@ -1,31 +1,37 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-function Heatmap(props:{fieldsData:any,className?:string})
-{
-  function convertNumToArray(num:number)
-  {
+function Heatmap(props: { fieldsData: any; className?: string }) {
+  function convertNumToArray(num: number) {
+    // convert number to array of 0s and 1s
     let res = [];
-    while(num != 0)
-    {
-      res.push((num & 1));
-      num = num >> 1;
+    let idx = 0;
+    while (idx < 32) {
+      if (num & (1 << idx)) {
+        res.push(1);
+      } else {
+        res.push(0);
+      }
+      idx++;
     }
     return res;
   }
-  useEffect(()=>{
-    let res = [];
-    if(!props.fieldsData)return;
-    for(let key in props.fieldsData?.history)
-    {
-      let curr = props.fieldsData?.history[key];
-      console.log(curr);
-    } 
-  },[props.fieldsData])
-  return (
-    <div className={`${props.className}`}>
 
-    </div>
-  )
+  useEffect(() => {
+    let res: any = {}; // will contain array of arrays of 0s and 1s for each month
+    if (!props.fieldsData) return;
+    for (let key in props.fieldsData?.history) {
+      let curr = props.fieldsData?.history[key];
+      res[key] = [];
+
+      for (let i = 0; i < curr.length; i++) {
+        let currArr = convertNumToArray(curr[i]);
+
+        res[key].push(currArr);
+      }
+    }
+    console.log(res);
+  }, [props.fieldsData]);
+  return <div className={`${props.className}`}></div>;
 }
 
-export default Heatmap
+export default Heatmap;
