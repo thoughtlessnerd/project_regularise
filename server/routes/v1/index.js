34dd11d28e9cdf8839208d6f0f3f93cb81e89f28
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-02-05 22:54:47 +0530

    cleaned up routes section a little

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -18,15 +18,8 @@ router.post("/field/done", isAuthenticated, fieldController.setDoneFields);
 router.delete("/field", isAuthenticated, fieldController.deleteField);
 
 router.get("/image", isAuthenticated, imageController.getProfileImage);
-
 router.post("/image", isAuthenticated, imageController.uploadProfileImage);
-
 router.put("/image", isAuthenticated, imageController.updateProfileImage);
-
 router.delete("/image", isAuthenticated, imageController.deleteProfileImage);
-
-// router.get("/test", isAuthenticated, (req, res) => {
-//   res.status(200).json({ message: "success", req: req.user });
-// });
 
 module.exports = router;
