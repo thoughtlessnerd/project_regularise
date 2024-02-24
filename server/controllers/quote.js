5cb1feb30d7ed55e87970cefbffa4e060219164b
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-02-24 15:23:28 +0530

    revert prev commit

--- server/controllers/quote.js
+++ server/controllers/quote.js
@@ -7,12 +7,11 @@ class quoteController {
 
   getQuoteForToday = async (req, res) => {
     try {
-      // const quote = await this.quoteService.getQuoteForToday();
-      return res.status(200).json({ message: "success", success: true, data: "quote" });
+      const quote = await this.quoteService.getQuoteForToday();
 
-      // return res
-      //   .status(200)
-      //   .json({ message: "success", success: true, data: quote });
+      return res
+        .status(200)
+        .json({ message: "success", success: true, data: quote });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
