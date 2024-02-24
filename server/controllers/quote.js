dca0c4eb8a1fbb2d48d5671e0ca19c765f3698fc
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-02-24 15:20:53 +0530

    checking api' '

--- server/controllers/quote.js
+++ server/controllers/quote.js
@@ -7,11 +7,12 @@ class quoteController {
 
   getQuoteForToday = async (req, res) => {
     try {
-      const quote = await this.quoteService.getQuoteForToday();
+      // const quote = await this.quoteService.getQuoteForToday();
+      return res.status(200).json({ message: "success", success: true, data: "quote" });
 
-      return res
-        .status(200)
-        .json({ message: "success", success: true, data: quote });
+      // return res
+      //   .status(200)
+      //   .json({ message: "success", success: true, data: quote });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
