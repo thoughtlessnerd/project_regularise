1510bf2290c6a3cb9f091d8ff7f0a403b3b3915f
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-07-29 15:43:15 +0530

    a logo now

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,33 +1,40 @@
-import { Link } from 'react-router-dom'
-import Button from './Button'
-import TextButton from './TextButton'
-import { useAuth } from '../context/AuthContext'
-import Timer from './Timer';
+import { Link } from "react-router-dom";
+import Button from "./Button";
+import TextButton from "./TextButton";
+import { useAuth } from "../context/AuthContext";
+import Timer from "./Timer";
 
-function Navbar(props:{className?:string})
-{
-    const auth = useAuth();
+function Navbar(props: { className?: string }) {
+  const auth = useAuth();
 
-    return (
-        <>
-            {
-            (!auth?.isAuthorized)?(
-                <nav className={`${props.className} container mx-auto p-8 flex justify-between items-center`}>
-                    <h1>REGULARISE</h1>
-                    <div className='flex gap-4'>
-                        <Link to='/signin'><TextButton className=''>SIGN IN</TextButton></Link>
-                        <Link to='/signup'><Button className='' color='primary'>SIGN UP</Button></Link>
-                    </div>
-                </nav>
-            ):(
-                <nav className={`${props.className} container mx-auto p-8 flex justify-between items-center`}>
-                    <h1>REGULARISE</h1>
-                    <Timer />
-                </nav>
-            )
-        }
-        </>
-    )
+  return (
+    <>
+      {!auth?.isAuthorized ? (
+        <nav
+          className={`${props.className} container mx-auto p-8 flex justify-between items-center`}
+        >
+          <h1 className="logo-font">REGULARISE</h1>
+          <div className="flex gap-4">
+            <Link to="/signin">
+              <TextButton className="">SIGN IN</TextButton>
+            </Link>
+            <Link to="/signup">
+              <Button className="" color="primary">
+                SIGN UP
+              </Button>
+            </Link>
+          </div>
+        </nav>
+      ) : (
+        <nav
+          className={`${props.className} container mx-auto p-8 flex justify-between items-center`}
+        >
+          <h1 className="logo-font text-3xl max-md:text-2xl">ReGULARISE</h1>
+          <Timer />
+        </nav>
+      )}
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
