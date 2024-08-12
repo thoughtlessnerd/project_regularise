9c16ae40eb2e54d1d420dde8c2e2c917181af640
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-08-12 22:32:16 +0530

    aws app runner as current server

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://x5k25nvwrm.ap-south-1.awsapprunner.com/api/v1",
+  baseURL: "https://z7vr6hebgs.us-east-1.awsapprunner.com/api/v1",
   timeout: 10000,
 });
 
